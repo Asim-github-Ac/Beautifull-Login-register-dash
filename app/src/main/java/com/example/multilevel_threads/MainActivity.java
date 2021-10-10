1879a1be40282d6dd3137c0dc843ff3234456ce7
Asim Mehar <asimmehar1931@gmail.com>
2021-10-10 09:31:11 -0700

    DashBoard With Cards

--- app/src/main/java/com/example/multilevel_threads/MainActivity.java
+++ app/src/main/java/com/example/multilevel_threads/MainActivity.java
@@ -2,16 +2,25 @@ package com.example.multilevel_threads;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initialization();
 
 
+
+
+    }
+    public void initialization(){
+
+        toolbar =findViewById(R.id.toolbar);
 
 
     }
